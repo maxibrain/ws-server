98077b945756a59348a6f300f57a2bc80c775880
Maxim Kostyrkin <mkostyrkin@gmail.com>
2019-12-28 17:21:12 +0200

    some logs added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 wss.on('connection', function connection(ws) {
+  console.log('connection', ws.url);
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
@@ -10,4 +11,7 @@ wss.on('connection', function connection(ws) {
       }
     });
   });
+  ws.on('close', function onClose(code, reason) {
+    console.log('close', { code, reason });
+  })
 });
