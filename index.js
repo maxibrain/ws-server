84e2e82de9590c4721e6830ecaba826c4463f1a5
Maxim Kostyrkin <mkostyrkin@gmail.com>
2019-12-28 17:30:07 +0200

    some changes

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
-wss.on('connection', function connection(ws) {
-  console.log('connection', ws.url);
+wss.on('connection', function connection(ws, req) {
+  const ip = req.connection.remoteAddress;
+  console.log(`${ip} connected at ${req.url}`);
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
@@ -11,7 +12,7 @@ wss.on('connection', function connection(ws) {
       }
     });
   });
-  ws.on('close', function onClose(code, reason) {
-    console.log('close', { code, reason });
+  ws.on('close', function onClose() {
+    console.log(`${ip} disconnected`);
   })
 });
